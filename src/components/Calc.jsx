79922324a2bb9a0a19255f802711c7a10aa7b78c
Shivam Dixit <archanavrindavanvlog@gmail.com>
2024-10-22 01:33:00 +0530

    added the implementation of createShare api in Calc.jsx and it will save our share info in mongodb database

--- src/components/Calc.jsx
+++ src/components/Calc.jsx
@@ -1,82 +1,154 @@
 import React, { useState } from "react";
 import { IoCloseCircleSharp } from "react-icons/io5";
 function Calc() {
-    const [person,setPerson] = useState([1]);
+  const [person, setPerson] = useState([]);
+  const [data, setData] = useState({});
+
+  const addShare = async () => {
+    console.log(data);
+    const response = await fetch(
+      "http://localhost:5000/api/share/createShare",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          ...data,
+          tag: data.tag.split(","),
+          person: person,
+        }),
+      }
+    );
+    if (response.ok) {
+      const json = response.json();
+      console.log(json);
+    }
+    console.log();
+  };
+
+  const updatePerson = (event, elem, index) => {
+    try {
+      const personData = { ...elem, [event.target.name]: event.target.value };
+      const newPerson = [...person];
+      newPerson[index] = personData;
+      setPerson(newPerson);
+    } catch (e) {
+      console.log({ e });
+    }
+  };
+  const updateData = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value });
+  };
   return (
     <div className="w-50 mx-auto bg-secondary mx-auto my-2 rounded p-3">
-      <div class="input-group input-group-sm mb-3 ">
-        <span class="input-group-text" id="inputGroup-sizing-sm">
+      <div className="input-group input-group-sm mb-3 ">
+        <span className="input-group-text" id="inputGroup-sizing-sm">
           Memory Title
         </span>
         <input
+          name="title"
+          onChange={updateData}
           type="text"
-          class="form-control"
+          className="form-control"
           aria-label="Sizing example input"
           aria-describedby="inputGroup-sizing-sm"
         />
-        <span class="input-group-text">Tag</span>
+        <span className="input-group-text">Tag</span>
         <input
+          name="tag"
+          onChange={updateData}
           type="text"
-          class="form-control"
+          className="form-control"
           placeholder="Tag(Ex:Food, Travel, Borrow)"
           aria-label="Server"
         />
-      </div> 
-      {
-            person.map((elem)=>{
-                return <div class="input-group mb-3 ">
-       
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder={elem+": Username"}
-                  aria-label="Username"
-                />
-                <span class="input-group-text">₹</span>
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="₹"
-                  aria-label="Server"
-                />
-        <span class="input-group-text">Desc</span>
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Desc"
-                  aria-label="Server"
-                />
-                <IoCloseCircleSharp size={30} className={person.length == elem && elem !=1 ?"d-unset text-dark mx-1 my-1":"d-none"} onClick={()=>{
-                    setPerson(person.slice(0,person.length-1));
-                }}/>
-              </div>
-            })
-        }
+      </div>
+      {person.map((elem, index) => {
+        return (
+          <div className="input-group mb-3 " key={index}>
+            <input
+              name="Username"
+              onChange={(event) => {
+                updatePerson(event, elem, index);
+              }}
+              type="text"
+              className="form-control"
+              placeholder={index + 1 + ": Username"}
+              aria-label="Username"
+            />
+            <span className="input-group-text">₹</span>
+            <input
+              name="Rs"
+              onChange={(event) => {
+                updatePerson(event, elem, index);
+              }}
+              type="text"
+              className="form-control"
+              placeholder="₹"
+              aria-label="Server"
+            />
+            <span className="input-group-text">Desc</span>
+            <input
+              name="Desc"
+              onChange={(event) => {
+                updatePerson(event, elem, index);
+              }}
+              type="text"
+              className="form-control"
+              placeholder="Desc"
+              aria-label="Server"
+            />
+            <IoCloseCircleSharp
+              size={30}
+              className={
+                person.length === index + 1 && index + 1 !== 1
+                  ? "d-unset text-dark mx-1 my-1"
+                  : "d-none"
+              }
+              onClick={() => {
+                setPerson(person.slice(0, person.length - 1));
+              }}
+            />
+          </div>
+        );
+      })}
 
-      
-        <div className="userNameRecomendation">
-          {/* Display will only be non none when no input will be in focus and when there is no text in input so it suggest random friends or recent userName used by account holder */}
-        </div>
-      <button class="btn btn-primary w-100 my-1" type="button" onClick={()=>{
-        setPerson([...person,person.length+1])
-      }}>Add Friend</button>
+      <div className="userNameRecomendation">
+        {/* Display will only be non none when no input will be in focus and when there is no text in input so it suggest random friends or recent userName used by account holder */}
+      </div>
+      <button
+        className="btn btn-primary w-100 my-1"
+        type="button"
+        onClick={() => {
+          setPerson([...person, { Username: "", Rs: 0, Desc: "" }]);
+        }}
+      >
+        Add Friend
+      </button>
       <div className="input-group my-1">
-          <input
-            type="file"
-            class="form-control"
-            id="inputGroupFile04"
-            aria-describedby="inputGroupFileAddon04"
-            aria-label="Upload"
-          />
-          <button
-            class="btn btn-outline-light"
-            type="button"
-            id="inputGroupFileAddon04"
-          >
-            View
-          </button>
-        </div>
-        <button class="btn btn-primary w-100 my-1" type="button">Create Share</button>
+        <input
+          type="file"
+          className="form-control"
+          id="inputGroupFile04"
+          aria-describedby="inputGroupFileAddon04"
+          aria-label="Upload"
+        />
+        <button
+          className="btn btn-outline-light"
+          type="button"
+          id="inputGroupFileAddon04"
+        >
+          View
+        </button>
+      </div>
+      <button
+        className="btn btn-primary w-100 my-1"
+        type="button"
+        onClick={addShare}
+      >
+        Create Share
+      </button>
     </div>
   );
 }
